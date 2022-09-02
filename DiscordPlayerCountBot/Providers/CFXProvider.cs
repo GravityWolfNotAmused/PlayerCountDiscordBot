414df1c85c100e5d5ca06c7df3ef9eaf913425cb
Larry <gravitywolfnotamused@gmail.com>
2022-09-02 11:08:40 -0600

    Update CFXProvider.cs
    Change Exception handling for CFX

--- DiscordPlayerCountBot/Providers/CFXProvider.cs
+++ DiscordPlayerCountBot/Providers/CFXProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using DiscordPlayerCountBot.Services;
 using log4net;
@@ -10,7 +11,7 @@ namespace DiscordPlayerCountBot.Providers
     {
         public ILog Logger = LogManager.GetLogger(typeof(SteamProvider));
 
-        public async Task<GenericServerInformation> GetServerInformation(BotInformation information)
+        public async Task<GenericServerInformation?> GetServerInformation(BotInformation information)
         {
             var service = new CFXService();
 
@@ -19,6 +20,12 @@ namespace DiscordPlayerCountBot.Providers
                 var playerInfo = await service.GetPlayerInformationAsync(information.Address);
                 var serverInfo = await service.GetServerInformationAsync(information.Address);
                 var addressAndPort = information.GetAddressAndPort();
+
+                if (playerInfo == null)
+                    throw new ApplicationException("Player Information cannot be null. Is your server offline?");
+
+                if (serverInfo == null)
+                    throw new ApplicationException("Server Information cannot be null. Is your server offline?");
 
                 return new GenericServerInformation()
                 {
@@ -30,6 +37,31 @@ namespace DiscordPlayerCountBot.Providers
             }
             catch(Exception e)
             {
+                if(e is WebException webException)
+                {
+                    if (webException.Status == WebExceptionStatus.Timeout)
+                    {
+                        Logger.Error($"[CFXProvider] - Speaking with CFX has timed out.", e);
+                        return null;
+                    }
+                    else if (webException.Status == WebExceptionStatus.ConnectFailure)
+                    {
+                        Logger.Error($"[CFXProvider] - Could not connect to CFX.");
+                        return null;
+                    }
+                    else
+                    {
+                        Logger.Error($"[CFXProvider] - There was an error speaking with your CFX server.", e);
+                        return null;
+                    }
+                }
+
+                if (e is ApplicationException applicationException)
+                {
+                    Logger.Error($"[CFXProvider] - {e.Message}");
+                    return null;
+                }
+
                 Logger.Error($"[CFXProvider] - There was an error speaking with CFX.", e);
                 throw;
             }
