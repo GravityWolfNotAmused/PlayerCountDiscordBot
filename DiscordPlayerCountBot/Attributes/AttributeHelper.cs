8e187a9bcd5759871c2defd66ede0438de779eb8
Larry <gravitywolfnotamused@gmail.com>
2022-09-09 17:44:41 -0600

    Create AttributeHelper.cs
    Makes getting stuff from attributes easier :)

---
+++ DiscordPlayerCountBot/Attributes/AttributeHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace DiscordPlayerCountBot.Attributes
+{
+    public static class AttributeHelper
+    {
+        public static string GetNameFromAttribute(object obj)
+        {
+            var nameAttribute = obj.GetType().GetCustomAttributes(true).Where(attribute => attribute.GetType() == typeof(NameAttribute)).Cast<NameAttribute>().FirstOrDefault();
+            var label = nameAttribute?.Name ?? obj.GetType().Name;
+            return label;
+        }
+    }
+}
