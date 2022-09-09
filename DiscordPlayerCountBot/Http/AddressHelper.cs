accd7423e8fb77d9178dec5d28844ea22b78c9b5
Larry <gravitywolfnotamused@gmail.com>
2022-09-09 17:47:23 -0600

    Create AddressHelper.cs

---
+++ DiscordPlayerCountBot/Http/AddressHelper.cs
@@ -0,0 +1,35 @@
+﻿using log4net.Repository.Hierarchy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace DiscordPlayerCountBot.Http
+{
+    public static class AddressHelper
+    {
+        public static async Task<string> GetHostAddress()
+        {
+            var publicIPAddress = string.Empty;
+            var httpClient = new HttpExecuter();
+            var ipAddress = await httpClient.GET<object, string>("http://ifconfig.me");
+
+            if (string.IsNullOrEmpty(ipAddress))
+                throw new ApplicationException("IP Address cannot be null. Host failed to resolve address.");
+
+            return ipAddress;
+        }
+
+        public static async Task<string> ResolveAddress(string address)
+        {
+            var splitAddr = address.Split(":");
+            var resolvedAddress = await GetHostAddress();
+            var port = splitAddr[1];
+            return resolvedAddress + ":" + port;
+        }
+    }
+}
