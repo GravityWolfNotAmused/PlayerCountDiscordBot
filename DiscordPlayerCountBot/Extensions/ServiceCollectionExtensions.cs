151395f0ca9aaeb8e37f552072337c52c206bcf4
Larry <gravitywolfnotamused@gmail.com>
2025-03-04 23:40:40 -0700

    Update ServiceCollectionExtensions.cs

--- DiscordPlayerCountBot/Extensions/ServiceCollectionExtensions.cs
+++ DiscordPlayerCountBot/Extensions/ServiceCollectionExtensions.cs
@@ -5,17 +5,18 @@ namespace DiscordPlayerCountBot.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddAllImplementationsOf<TInterface>(this IServiceCollection services, bool isTransient = false, params Assembly[] assemblies)
+        public static IServiceCollection AddAllImplementationsOf<TInterface>(this IServiceCollection services, bool isTransient = false)
         {
-            if (assemblies == null || assemblies.Length == 0)
-                assemblies = [Assembly.GetEntryAssembly()!, Assembly.GetCallingAssembly(), Assembly.GetExecutingAssembly()];
+            var assemblies = new Assembly[] { Assembly.GetEntryAssembly()!, Assembly.GetCallingAssembly(), Assembly.GetExecutingAssembly() };
 
             var implementationTypes = assemblies.SelectMany(a => a.GetTypes())
                 .Distinct()
                 .Where(t => typeof(TInterface).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract)
                 .ToList();
 
+#if DEBUG
             Console.WriteLine($"Registering: {implementationTypes.Count} {typeof(TInterface).Name}");
+#endif
 
             foreach (var type in implementationTypes)
             {
